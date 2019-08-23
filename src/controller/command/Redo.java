a49d6b58d5e4285f8cb3764359cf52644ea46289
Luis Norman <lanorman14@gmail.com>
2019-08-23 09:15:30 -0500

    Corrected the display messages.

--- src/controller/command/Redo.java
+++ src/controller/command/Redo.java
@@ -21,43 +21,44 @@ public class Redo implements ICommand{
 
         switch(lastCommand.getCommandName()) {
             case "Draw":
+                System.out.println("Redoing draw");
                 DrawCommand mostRecentDrawCommand = (DrawCommand)lastCommand;
                 mostRecentDrawCommand.redo();
                 break;
 
             case "Paste":
-                System.out.println("Undoing paste");
+                System.out.println("Redoing paste");
                 PasteCommand mostRecentPasteCommand = (PasteCommand)lastCommand;
                 mostRecentPasteCommand.redo();
                 break;
 
             case "Move":
-                System.out.println("Undoing move");
+                System.out.println("Redoing move");
                 MoveCommand mostRecentMoveCommand = (MoveCommand)lastCommand;
                 mostRecentMoveCommand.redo();
                 break;
 
             case "Group":
-                System.out.println("Undoing group");
+                System.out.println("Redoing group");
                 GroupCommand mostRecentGroupCommand = (GroupCommand)lastCommand;
                 mostRecentGroupCommand.redo();
                 break;
 
             case "Ungroup":
-                System.out.println("Undoing ungroup");
+                System.out.println("Redoing ungroup");
                 // Get ungrouped shapes and the group it belonged to;
                 UngroupCommand mostRecentUngroupCommand = (UngroupCommand)lastCommand;
                 mostRecentUngroupCommand.redo();
                 break;
 
             case "Delete":
-                System.out.println("Undoing delete");
+                System.out.println("Redoing delete");
                 DeleteCommand mostRecentDeleteCommand = (DeleteCommand)lastCommand;
                 mostRecentDeleteCommand.redo();
                 break;
 
             default:
-                System.out.println("Can't undo "+lastCommand.getCommandName());
+                System.out.println("Can't redo "+lastCommand.getCommandName());
         }
     }
 
